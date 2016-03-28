9637f099e9d1cea40191a55f558af33fc9085907
Melvinchun <melvin.manuel.portillo@gmail.com>
2016-03-28 16:13:31 -0600

    heroku

--- app/scripts/services/authService.js
+++ app/scripts/services/authService.js
@@ -1,7 +1,7 @@
 angular.module('AngularScaffold.Services').factory('AuthService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'mongodb://admin:admin@ds015899.mlab.com:15899/';
+		var baseUrl = 'http://scan-backend.herokuapp.com/#/';
 		return {
 				Logout: function(){
 					return $http.get(baseUrl + "v1/logout");
