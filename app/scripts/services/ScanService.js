da724df30b7b7806962c03354185ddb4a76d33c8
Melvinchun <melvin.manuel.portillo@gmail.com>
2016-03-28 15:46:45 -0600

    heroku

--- app/scripts/services/ScanService.js
+++ app/scripts/services/ScanService.js
@@ -1,7 +1,8 @@
 angular.module('AngularScaffold.Services').factory('ScanService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'http://localhost:8000/';
+		//var baseUrl = 'http://localhost:8000/';
+		var baseUrl = 'mongodb://admin:admin@ds015899.mlab.com:15899/';
 		return {
 				PostUser: function(payload){
 					return $http.post(baseUrl + "v1/register", payload);
