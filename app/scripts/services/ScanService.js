e6e0f516d9fcd4e74544b2bdfb2d26b2e040e628
Isaias Valle <guitartsword@gmail.com>
2016-03-31 17:57:49 -0600

    Update URL

--- app/scripts/services/ScanService.js
+++ app/scripts/services/ScanService.js
@@ -1,8 +1,8 @@
 angular.module('AngularScaffold.Services').factory('ScanService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'http://localhost:8000/';
-		//var baseUrl = 'https://scan-backend.herokuapp.com/';
+		//var baseUrl = 'http://localhost:8000/';
+		var baseUrl = 'https://scan-backend.herokuapp.com/';
 		return {
 				PostUser: function(payload){
 					return $http.post(baseUrl + "v1/register", payload);
